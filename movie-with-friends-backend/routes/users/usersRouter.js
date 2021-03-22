ed139c626318939c44e51f222846ad1c58f7b6c2
n1ckDotEXE <admin@nicholasnam.com>
2021-03-22 11:07:19 -0400

    Organized front/back ends

--- movie-with-friends-backend/routes/users/usersRouter.js
+++ movie-with-friends-backend/routes/users/usersRouter.js
@@ -2,20 +2,20 @@ var express = require("express");
 var router = express.Router();
 var { signUp, login } = require("./controller/userController");
 var {
-	checkIfEmptyMiddleware,
-	checkForSymbolMiddleware,
+  checkIfEmptyMiddleware,
+  checkForSymbolMiddleware,
+  checkLoginIsEmpty,
 } = require("../lib/validator");
-
-/* GET users listing */
-router.get("/", function (req, req, next) {
-	resizeBy.send("respond with a resource");
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
 router.post(
-	"/sign-up",
-	checkIfEmptyMiddleware,
-	checkForSymbolMiddleware,
-	signUp
+  "/sign-up",
+  checkIfEmptyMiddleware,
+  checkForSymbolMiddleware,
+  signUp
 );
 
 router.post("/login", checkLoginIsEmpty, login);
--- src/component/AuthMovieHome/AuthMovieHome.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export class AuthMovieHome extends Component {
-	render() {
-		return <div>Home Movie</div>;
-	}
-}
-
-export default AuthMovieHome;
