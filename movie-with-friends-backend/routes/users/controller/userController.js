ea9d491a6bb0e391a183563fb43bb924edc83cb6
n1ckDotEXE <admin@nicholasnam.com>
2021-03-23 12:25:56 -0400

    TDeletes frontend to fix git

--- movie-with-friends-backend/routes/users/controller/userController.js
+++ movie-with-friends-backend/routes/users/controller/userController.js
@@ -5,61 +5,64 @@ const User = require("../Model/User");
 const mongoDBErrorHelper = require("../../lib/mongoDBErrorHelper");
 
 module.exports = {
-  signUp: async (req, res) => {
-    try {
-      let salted = await bcrypt.genSalt(10);
-      let hashedPassword = await bcrypt.hash(req.body.password, salted);
+	signUp: async (req, res) => {
+		try {
+			let salted = await bcrypt.genSalt(10);
+			let hashedPassword = await bcrypt.hash(req.body.password, salted);
 
-      let createdUser = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        password: hashedPassword,
-      });
+			let createdUser = new User({
+				firstName: req.body.firstName,
+				lastName: req.body.lastName,
+				email: req.body.email,
+				password: hashedPassword,
+			});
 
-      let savedUser = await createdUser.save();
+			let savedUser = await createdUser.save();
 
-      res.json({
-        data: savedUser,
-      });
-    } catch (e) {
-      res.status(500).json(mongoDBErrorHelper(e));
-      // res.status(500).json({
-      //   message: e.message,
-      // });
-    }
-  },
-  login: async (req, res) => {
-    try {
-      let foundUser = await User.findOne({ email: req.body.email });
+			res.json({
+				data: savedUser,
+			});
+		} catch (e) {
+			res.status(500).json(mongoDBErrorHelper(e));
+			// res.status(500).json({
+			//   message: e.message,
+			// });
+		}
+	},
+	login: async (req, res) => {
+		try {
+			let foundUser = await User.findOne({ email: req.body.email });
 
-      if (!foundUser) {
-        throw { message: "Email is not registered, please go sign up!" };
-      }
+			if (!foundUser) {
+				throw {
+					message: "Email is not registered, please go sign up!",
+				};
+			}
 
-      let comparedPassword = await bcrypt.compare(
-        req.body.password,
-        foundUser.password
-      );
+			let comparedPassword = await bcrypt.compare(
+				req.body.password,
+				foundUser.password
+			);
 
-      if (!comparedPassword) {
-        throw { message: "Check your email and password!" };
-      } else {
-        let jwtToken = jwt.sign(
-          {
-            email: foundUser.email,
-          },
-          "mightyhamster",
-          { expiresIn: "1d" }
-        );
+			if (!comparedPassword) {
+				throw { message: "Check your email and password!" };
+			} else {
+				let jwtToken = jwt.sign(
+					{
+						email: foundUser.email,
+					},
+					"mightyhamster",
+					{ expiresIn: "1d" }
+				);
 
-        res.json({
-          jwtToken: jwtToken,
-        });
-      }
-    } catch (e) {
-      console.log(39, e);
-      res.status(500).json(mongoDBErrorHelper(e));
-    }
-  },
+				res.json({
+					jwtToken: jwtToken,
+				});
+			}
+		} catch (e) {
+			console.log(39, e);
+			res.status(500).json(mongoDBErrorHelper(e));
+		}
+	},
+	updatedUserPassword: async () => {},
 };
--- movie-with-friends-backend/routes/users/usersRouter.js
+++ movie-with-friends-backend/routes/users/usersRouter.js
@@ -2,22 +2,24 @@ var express = require("express");
 var router = express.Router();
 var { signUp, login } = require("./controller/userController");
 var {
-  checkIfEmptyMiddleware,
-  checkForSymbolMiddleware,
-  checkLoginIsEmpty,
+	checkIfEmptyMiddleware,
+	checkForSymbolMiddleware,
+	checkLoginIsEmpty,
 } = require("../lib/validator");
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+	res.send("respond with a resource");
 });
 
 router.post(
-  "/sign-up",
-  checkIfEmptyMiddleware,
-  checkForSymbolMiddleware,
-  signUp
+	"/sign-up",
+	checkIfEmptyMiddleware,
+	checkForSymbolMiddleware,
+	signUp
 );
 
 router.post("/login", checkLoginIsEmpty, login);
+
+router.put("/update-user-password", updateUserPassword);
 
 module.exports = router;
